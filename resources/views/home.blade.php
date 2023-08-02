d6fcf430283864e3aa3a09b2ac2da0ce07ede716
TALEX1995 <tanase.alex@libero.it>
2023-08-02 15:28:49 +0200

    Variable pass with route

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,6 +6,6 @@
     <title>Laravel</title>
 </head>
 <body>
-    <h1>Hello World</h1>
+    <h1>{{$phrase}}</h1>
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    $phrase = 'Hello World';
+    return view('home', compact('phrase'));
 });
